61a460e8877336fa204f03892adeb7e21ce623b0
Kiazee <23-01342@g.batstate-u.edu.ph>
2025-04-24 13:30:15 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        print(request.form['name'], request.form['message'])
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
